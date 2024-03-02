1c4a2f503f1db28db4225e2b9783c96ad692b9a1
AndreaParrini <andrea.parrini26@gmail.com>
2024-03-02 14:12:53 +0100

    add link to bootstrap for all files
    add class on file index.php

--- index.php
+++ index.php
@@ -3,17 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Badwords</title>
 </head>
 <body>
-    <form action="./results_form.php" method="get">
-        <label for="paragraph">insert a paragraph</label>        
-        <input type="text" name="paragraph" id="paragraph">
-
-        <label for="badword">insert a word to censure</label>        
-        <input type="text" name="badword" id="badword">
-
-        <button type="submit">Send</button>
-    </form>
+    <div class="container p-3">
+        <form action="./results_form.php" method="get">
+            <div class="mb-3">
+                <label for="paragraph" class="form-label">insert a paragraph</label>        
+                <input type="text" class="form-control" name="paragraph" id="paragraph">
+            </div>
+            
+            <div class="mb-3">
+                <label for="badword" class="form-label">insert a word to censure</label>        
+                <input type="text" class="form-control" name="badword" id="badword">
+            </div>
+            
+            <button type="submit" class="btn btn-primary">Send</button>
+        </form>
+    </div>
+    
 </body>
 </html>
--- results_form.php
+++ results_form.php
@@ -18,6 +18,7 @@ if (!isset($_GET['paragraph']) && !isset($_GET['badword'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Badwords</title>
 </head>
 <body>
